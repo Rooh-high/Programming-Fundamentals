9ea5872323d71d77bae08ff1bebf772380273e6e
Horriya Noor <64048@students.riphah.edu.pk>
2024-10-25 12:55:14 -0700

    Add files via upload

---
+++ Counting 1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"\n10" ;
+	cout<<"\n9 ";
+	cout<<"\n8";
+	cout<<"\n7 ";
+	cout<<"\n6 ";
+	cout<<"\n5";
+	cout<<"\n4";
+	cout<<"\n3 ";
+	cout<<"\n2";
+	cout<<"\n1 "<<endl;
+	
+    cout<<"10"<<endl ;
+	cout<<"9 "<<endl;
+	cout<<"8"<<endl;
+	cout<<"7 "<<endl;
+	cout<<"6 "<<endl;
+	cout<<"5"<<endl;
+	cout<<"4"<<endl;
+	cout<<"3 "<<endl;
+	cout<<"2"<<endl;
+	cout<<"1 "<<endl;
+	return 0;
+ } 
+ 
---
+++ Intro 3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	 string name = "horriya"; 
+	 string ID = "64048";
+	 string semester = "1st Semester";
+	string course= "Computr science" ;
+		
+	cout<<"Name : " << name<<endl;
+	cout<<"Sap ID: "<< ID<<endl;
+	cout<<"Semester: "<<semester<<endl;
+	cout<<"Course: "<<course<<endl; 
+	return 0;
+}
